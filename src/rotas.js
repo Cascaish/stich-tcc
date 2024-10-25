1e2c1b2646c82171f8c145a5860512b102485718
x4nde123 <alexandrelopes28794512@gmail.com>
2024-10-25 07:27:34 -0300

    alterado

--- src/rotas.js
+++ src/rotas.js
@@ -1,12 +1,13 @@
 import apiControllerUsuario from './controller/apiControllerUsuario.js'
 import apiControllerClientes from './controller/apiControllerClientes.js'
+import apiControllerVendas from './controller/apiControllerVendas.js'
 
 
 export function rotas(servidor){
 
     servidor.use(apiControllerUsuario)
     servidor.use(apiControllerClientes)
-
+    servidor.use(apiControllerVendas)
 
 }
 
